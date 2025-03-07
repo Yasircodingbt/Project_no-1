df752f856e79ab3bafb81957216a8a53e1d01c6d
Yasircondingbt <185803128+Yasircodingbt@users.noreply.github.com>
2025-03-07 18:53:39 +0500

    This project collected the data with simple way and save them in file pointer

--- FSc_lan.c
+++ FSc_lan.c
@@ -1,37 +1,64 @@
 #include<stdio.h>
 #include"string.h"
-#include<math.h>
-#include<time.h>
-typedef struct ful_house{
+#include"main.h"
+typedef struct collection_of_data{
+    char myself[200];
     char father[20];
-    char familes[100];
-    int age[50];
-    char sister[100];
-    char bother[100];
-    char member[100];
-    char business[500];
-     char mother[20];
-     char name[25];
-     int class;
-     char school[50];
-     char collage[50];
-     char uni[50];
+    char business[100];
+    char name[25];
+    char education[100];
+    int age;
+}cof;
+void father(FILE *fptr, cof data[]);
+void myself(FILE *fptr,cof data[]);
 
-}fh;
-void home();
+
+
 int main(void){
+  cof data[10];
   FILE *fptr;
-  fptr = fopen("home_file.txt","a");
-   
- 
-
-
+  fptr = fopen("textual.txt","w");
+    if (fptr == NULL){
+      printf("\033[1;36m File doesn't exist\033[0m");
+    }else{ 
+      myself(fptr,data);       
+      father(fptr,data);
+    }
    fclose(fptr);
    return 0;
    }
-void home(){
 
 
 
-    
+void father(FILE *fptr,cof data[]){
+ printf("\033[1;35mDetail of Father \n\033[0m");
+    fprintf(fptr,"Detail of Father\n");
+      printf("\033[1;33mEnter the Name of Father \n\033[0m");
+        fgets(data->name,25,stdin);
+            fprintf(fptr,"Name of Father %s",data->name);
+              puts("\033[1;37mEnter the Business of Father\033[0m"); 
+                  fgets(data->business,100,stdin);
+                     fprintf(fptr,"Business of Father : \n%s",data->business);
+                        puts("\033[1;32mEnter the Age of Father\033[0m");
+                           scanf("%d",&data->age);
+                              getchar();
+                               fprintf(fptr,"Age of Father %d year",data->age);
+           return;
+}
+void myself(FILE *fptr,cof data[]){
+
+  printf("\033[1;31mInterdoucing to Myself\n\033[0m");
+   fprintf(fptr,"Inderducing to Myself : \n");
+      fgets(data->myself,200,stdin);
+         fprintf(fptr,"%s",data->myself);
+           puts("\033[1;34m What is you Age\033[0m");
+              fprintf(fptr,"What is your Age\n");
+                scanf("%d",&data->age);
+                  fprintf(fptr,"My age is %d year ",data->age);
+                    getchar();
+                      puts("\033[1;35m About education \033[0m");
+                        fprintf(fptr,"About education\n");
+                          fgets(data->education,100,stdin);          
+                            fprintf(fptr,"%s",data->education);    
+         return;
 }
---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MATH_H
+
+int power(int a, int p);
+int _putchar(char *s);
+
+
+
+#endif
